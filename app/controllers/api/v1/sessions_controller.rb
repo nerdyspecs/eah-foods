bea6698cbcdbf1fe4355020651932dae1500c862
jupiter <lthong96@gmail.com>
2020-10-08 05:02:40 +0800

    removing byebug from sessions_controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,7 +6,6 @@ class Api::V1::SessionsController < Devise::SessionsController
   def create
     if @user.valid_password?(sign_in_params[:password])
       sign_in "user", @user
-      byebug
       render json: payload(@user), status: :ok
     else
       render json: {
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if User.all.empty?
-	for i in (1..2)
-		User1 = User.new(:email=>"user1@mail.com", :password=>"user1password")
-		User2 = User.new(:email=>"user2@mail.com", :password=>"user2password")
-		if User1.save
-			puts "User1 created"
-			AuditLog.create(:user=>"teckhong", :action=>"seeding user1 as regualar user", :description=>"")
-		end
-		if User2.save
-			puts "User2 created"
-			AuditLog.create(:user=>"teckhong", :action=>"seeding user2 as regualar user", :description=>"")
-		end
+	User1 = User.new(:email=>"user1@mail.com", :password=>"user1password")
+	User2 = User.new(:email=>"user2@mail.com", :password=>"user2password")
+	if User1.save
+		puts "User1 created"
+		AuditLog.create(:user=>"teckhong", :action=>"seeding user1 as regualar user", :description=>"")
+	end
+	if User2.save
+		puts "User2 created"
+		AuditLog.create(:user=>"teckhong", :action=>"seeding user2 as regualar user", :description=>"")
 	end
 end
 
